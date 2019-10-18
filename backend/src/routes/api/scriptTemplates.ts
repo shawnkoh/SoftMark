f26a989c01dab0b413b8d6c1f03025980bae7afe
Shawn Koh <shawnkoh@me.com>
2019-10-18 23:04:17 +0800

    Add script_templates routes

---
+++ backend/src/routes/api/scriptTemplates.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import * as ScriptTemplatesController from "../../controllers/ScriptTemplatesController";
+import { checkBearerToken } from "../../middlewares/checkBearerToken";
+import { BearerTokenType } from "../../types/tokens";
+
+export const router = Router();
+
+router.use(checkBearerToken(BearerTokenType.AccessToken));
+router.post("/", ScriptTemplatesController.create);
+router.get("/:id", ScriptTemplatesController.show);
+
+export default router;
