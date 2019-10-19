88900468d35861ca04f81e730a41c79e43be3e1c
Shawn Koh <shawnkoh@me.com>
2019-10-19 17:27:14 +0800

    Update email methods

--- backend/src/utils/sendgrid.ts
+++ backend/src/utils/sendgrid.ts
@@ -2,21 +2,28 @@ import * as jwt from "jsonwebtoken";
 import * as sendgrid from "@sendgrid/mail";
 import { MailData } from "@sendgrid/helpers/classes/mail";
 import { User } from "../entities/User";
-import { Script } from "../entities/Script";
 import { PaperUser } from "../entities/PaperUser";
 import {
-  AccessTokenSignedPayload,
   BearerTokenType,
   ResetPasswordTokenPayload
 } from "../types/tokens";
 
 const baseUrl = "https://nus.reviews";
 
-export function sendVerificationEmail(user: User) {
+function send(user: User, subject: string, message: string) {
   sendgrid.setApiKey(process.env.SENDGRID_API_KEY!);
+  const data: MailData = {
+    to: user.email,
+    from: "mail@nus.reviews",
+    subject,
+    text: message,
+    html: message
+  };
+  sendgrid.send(data);
+}
 
+export function sendVerificationEmail(user: User) {
   const payload = user.createPayload();
-
   const token = jwt.sign(payload, process.env.JWT_SECRET!, {
     expiresIn: "7 days"
   });
@@ -26,25 +33,24 @@ export function sendVerificationEmail(user: User) {
     "<p>We're excited that you're joining us! Because we're here to help you make marking exams great again, we want to get you up to speed quickly so that you can make the most of everything at cs3216-final-project</P>" +
     `<p>To get started, please <a href='${baseUrl}/auth/verify-email/${token}'>verify your email now!</a></p>`;
 
-  const msg: MailData = {
-    to: user.email,
-    from: "mail@nus.reviews",
-    subject: "Welcome to cs3216-final-project!",
-    html: message
-  };
-  sendgrid.send(msg);
+  send(user, "Welcome to cs3216-final-project!", message);
+}
+
+export function sendPasswordlessLoginEmail(user: User) {
+  const token = user.createAuthorizationToken();
+
+  const message = `<p>You may login using this token ${token}`;
+
+  send(user, "[cs3216-final-project] Passwordless Login", message);
 }
 
 export function sendInviteEmail(paperUser: PaperUser) {}
 
 export function sendResetPasswordEmail(user: User) {
-  sendgrid.setApiKey(process.env.SENDGRID_API_KEY!);
-
   const payload: ResetPasswordTokenPayload = {
     type: BearerTokenType.ResetPasswordToken,
     ...user.getCredentials()
   };
-
   const token = jwt.sign(payload, process.env.JWT_SECRET!, {
     expiresIn: "3 hours"
   });
@@ -58,40 +64,18 @@ export function sendResetPasswordEmail(user: User) {
     "<p>Thanks,<br />" +
     "The cs3216-final-project team`</p>";
 
-  const data: MailData = {
-    to: user.email,
-    from: "mail@nus.reviews",
-    subject: "[cs3216-final-project] Please reset your password",
-    text: message,
-    html: message
-  };
-
-  sendgrid.send(data);
+  send(user, "[cs3216-final-project] Please reset your password", message);
 }
 
-export function sendScriptEmail(
-  accessTokenSignedPayload: AccessTokenSignedPayload,
-  script: Script
-) {
-  sendgrid.setApiKey(process.env.SENDGRID_API_KEY!);
-
-  const token = jwt.sign(script.createPayload(), process.env.JWT_SECRET!, {
-    expiresIn: "7 days"
-  });
+export function sendScriptEmail(user: User) {
+  const token = user.createAuthorizationToken();
 
   const message =
     `<p>Dear ...</p>` +
     "<p>You may view your [paper] script here.</p>" +
-    "<p>Please take note that you will not be able to view your script should you lose this special link.</p>" +
     "<br />" +
-    `<p>You may view it by <a href='${baseUrl}/scripts/${token}'>clicking on this link</a></p>`;
+    `<p>You may view it by <a href='${baseUrl}/scripts/${token}'>clicking on this link</a></p>` +
+    `<p>Alternatively, you may log into your email at ...URL... to view the script</p>`;
 
-  const msg: MailData = {
-    to: accessTokenSignedPayload.email,
-    from: "mail@nus.reviews",
-    subject: `[cs3216-final-project] View your [Paper] marks here!`,
-    text: message,
-    html: message
-  };
-  sendgrid.send(msg);
+  send(user, "[cs3216-final-project] View your [Paper] marks here!", message);
 }
