50df4be20326ddac5c2cecb61ebebaf71ab0d932
Shawn Koh <shawnkoh@me.com>
2019-10-19 17:45:41 +0800

    Update initial migration

--- backend/src/migrations/1571150302537-Initial.ts
+++ backend/src/migrations/1571150302537-Initial.ts
@@ -7,6 +7,7 @@ export class Initial1571150302537 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<any> {
       await getRepository(User).save([
         createUser("shawnkoh@me.com"),
+        createUser("shawnkoh@u.nus.edu", false),
         createUser("ooimingsheng@gmail.com"),
         createUser("rlrh1996@gmail.com"),
         createUser("fungsiqi07@gmail.com"),
@@ -18,9 +19,11 @@ export class Initial1571150302537 implements MigrationInterface {
 
 }
 
-function createUser(email: string): User {
+function createUser(email: string, password: boolean = true): User {
   let user = new User();
-  user.password = hashSync("setMeUp?");
+  if (password) {
+    user.password = hashSync("setMeUp?");
+  }
   user.email = email;
   user.emailVerified = true;
   return user;
