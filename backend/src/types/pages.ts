8474f7226ce7f0e48fb964f9406116fabbedb887
Shawn Koh <shawnkoh@me.com>
2019-10-20 16:18:06 +0800

    Add annotation, pageQuestion and page type scaffolding

---
+++ backend/src/types/pages.ts
@@ -0,0 +1,7 @@
+import { DiscardableData } from "./entities";
+
+export interface PagePostData {}
+
+export interface PageListData extends DiscardableData {}
+
+export interface PageData extends PageListData {}
