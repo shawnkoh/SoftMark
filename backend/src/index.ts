753bc75fa692a784ac3d48bf44ed678e508a4540
Shawn Koh <shawnkoh@me.com>
2019-10-22 01:41:35 +0800

    Separate creating server from index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,29 +1,6 @@
-import * as bodyParser from "body-parser";
-import * as cors from "cors";
-import * as express from "express";
-import * as helmet from "helmet";
-import * as morgan from "morgan";
-import "reflect-metadata";
-import { createConnection } from "typeorm";
-import routes from "./routes";
-import ormconfig from "../ormconfig";
+import { ApiServer } from "./server";
 
-createConnection(ormconfig)
-  .then(async connection => {
-    const app = express();
+const apiServer = new ApiServer();
+apiServer.initialize();
 
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(cors());
-    app.use(helmet());
-    app.use(morgan("dev"));
-
-    app.use("/", routes);
-
-    app.listen(3000);
-
-    console.log(
-      "Express server has started on port 3000. Open http://localhost:3000/users to see results"
-    );
-  })
-  .catch(error => console.log(error));
+export default apiServer;
