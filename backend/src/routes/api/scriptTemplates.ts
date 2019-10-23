b0cbd1db07ac8d91e3d39ce1d4498c38f7e027d8
Shawn Koh <shawnkoh@me.com>
2019-10-24 00:36:02 +0800

    Update script template routes

--- backend/src/routes/api/scriptTemplates.ts
+++ backend/src/routes/api/scriptTemplates.ts
@@ -6,11 +6,8 @@ import { BearerTokenType } from "../../types/tokens";
 export const router = Router();
 
 router.use(checkBearerToken(BearerTokenType.AccessToken));
-router.patch("/script_templates/:id", ScriptTemplatesController.update);
-router.delete("/script_templates/:id", ScriptTemplatesController.discard);
-router.patch(
-  "/script_templates/:id/undiscard",
-  ScriptTemplatesController.undiscard
-);
+router.patch("/:id", ScriptTemplatesController.update);
+router.delete("/:id", ScriptTemplatesController.discard);
+router.patch("/:id/undiscard", ScriptTemplatesController.undiscard);
 
 export default router;
