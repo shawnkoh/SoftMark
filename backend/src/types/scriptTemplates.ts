ca59b7a05bf01557461ba9ac3fa8668cc5dfcd6b
Shawn Koh <shawnkoh@me.com>
2019-10-24 00:35:52 +0800

    Update Script Template types

--- backend/src/types/scriptTemplates.ts
+++ backend/src/types/scriptTemplates.ts
@@ -8,6 +8,8 @@ export interface ScriptTemplatePostData {
   questionTemplates: QuestionTemplatePostData[];
 }
 
+export type ScriptTemplatePatchData = Partial<ScriptTemplatePostData>;
+
 export interface ScriptTemplateData extends DiscardableData {
   questionTemplates: QuestionTemplateListData[];
 }
