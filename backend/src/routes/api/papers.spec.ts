9ba1c9beba4f395e7cb2b60050d44a76c3020a47
Shawn Koh <shawnkoh@me.com>
2019-10-24 17:47:05 +0800

    Add papers api specs

--- backend/src/routes/api/papers.spec.ts
+++ backend/src/routes/api/papers.spec.ts
@@ -10,123 +10,107 @@ import {
 import { ApiServer } from "../../server";
 
 let server: ApiServer;
+let ownerAccessToken: string;
+let markerAccessToken: string;
+let studentAccessToken: string;
 beforeAll(async () => {
   server = new ApiServer();
   await server.initialize();
   await synchronize(server);
   await loadFixtures(server);
+  ownerAccessToken =
+    "Bearer " +
+    (await getToken(server, "owner@u.nus.edu", "setMeUp?")).accessToken;
+  markerAccessToken =
+    "Bearer " +
+    (await getToken(server, "marker@u.nus.edu", "setMeUp?")).accessToken;
+  studentAccessToken = "Bearer " + (await getStudentAccessToken());
 });
 
 afterAll(async () => {
   await server.close();
 });
 
+const getStudentAccessToken = async () => {
+  const authorizationToken = await getAuthorizationToken(
+    server,
+    "student@u.nus.edu"
+  );
+  const { accessToken } = await getPasswordlessToken(
+    server,
+    authorizationToken
+  );
+  return accessToken;
+};
+
 describe("POST papers/:id/users", () => {
-  it("should allow an Owner to access this route if he owns the paper", async () => {
-    const { accessToken } = await getToken(
-      server,
-      "owner@u.nus.edu",
-      "setMeUp?"
-    );
+  it("should allow a Paper's Owner to access this route", async () => {
     const validResponse = await request(server.server)
       .post("/v1/papers/1/users")
-      .set("Authorization", `Bearer ${accessToken}`)
+      .set("Authorization", ownerAccessToken)
       .send();
     expect(validResponse.status).not.toEqual(404);
 
     const rejectResponse = await request(server.server)
       .post("/v1/papers/2/users")
-      .set("Authorization", `Bearer ${accessToken}`)
+      .set("Authorization", ownerAccessToken)
       .send();
     expect(rejectResponse.status).toEqual(404);
   });
 
-  it("should not allow a Marker to access this route", async () => {
-    const { accessToken } = await getToken(
-      server,
-      "marker@u.nus.edu",
-      "setMeUp?"
-    );
-    const res = await request(server.server)
+  it("should not allow a Paper's Marker to access this route", async () => {
+    const response = await request(server.server)
       .post("/v1/papers/1/users")
-      .set("Authorization", `Bearer ${accessToken}`)
+      .set("Authorization", markerAccessToken)
       .send();
-    expect(res.status).toEqual(404);
+    expect(response.status).toEqual(404);
   });
 
-  it("should not allow a Student to access this route", async () => {
-    const authorizationToken = await getAuthorizationToken(
-      server,
-      "student@u.nus.edu"
-    );
-    const { accessToken } = await getPasswordlessToken(
-      server,
-      authorizationToken
-    );
-    const res = await request(server.server)
+  it("should not allow a Paper's Student to access this route", async () => {
+    const response = await request(server.server)
       .post("/v1/papers/1/users")
-      .set("Authorization", `Bearer ${accessToken}`)
+      .set("Authorization", studentAccessToken)
       .send();
-    expect(res.status).toEqual(404);
+    expect(response.status).toEqual(404);
   });
 
-  it("should allow an Owner to create another Owner", async () => {
-    const { accessToken } = await getToken(
-      server,
-      "owner@u.nus.edu",
-      "setMeUp?"
-    );
-    const res = await request(server.server)
+  it("should allow a Paper's Owner to create another Owner", async () => {
+    const response = await request(server.server)
       .post("/v1/papers/1/users")
-      .set("Authorization", `Bearer ${accessToken}`)
+      .set("Authorization", ownerAccessToken)
       .send({
         email: "owner2@u.nus.edu",
         role: PaperUserRole.Owner
       });
-    expect(res.status).toEqual(201);
+    expect(response.status).toEqual(201);
   });
 
-  it("should allow an Owner to create another Marker", async () => {
-    const { accessToken } = await getToken(
-      server,
-      "owner@u.nus.edu",
-      "setMeUp?"
-    );
-    const res = await request(server.server)
+  it("should allow a Paper's Owner to create another Marker", async () => {
+    const response = await request(server.server)
       .post("/v1/papers/1/users")
-      .set("Authorization", `Bearer ${accessToken}`)
+      .set("Authorization", ownerAccessToken)
       .send({
         email: "marker2@u.nus.edu",
         role: PaperUserRole.Marker
       });
-    expect(res.status).toEqual(201);
+    expect(response.status).toEqual(201);
   });
 
-  it("should allow an Owner to create another Student", async () => {
-    const { accessToken } = await getToken(
-      server,
-      "owner@u.nus.edu",
-      "setMeUp?"
-    );
-    const res = await request(server.server)
+  it("should allow a Paper's Owner to create another Student", async () => {
+    const response = await request(server.server)
       .post("/v1/papers/1/users")
-      .set("Authorization", `Bearer ${accessToken}`)
+      .set("Authorization", ownerAccessToken)
       .send({
         email: "student2@u.nus.edu",
         role: PaperUserRole.Student
       });
-    expect(res.status).toEqual(201);
+    expect(response.status).toEqual(201);
   });
 
   it("should not allow creating a duplicate PaperUser", async () => {
-    const { accessToken } = await getToken(
-      server,
-      "owner@u.nus.edu",
-      "setMeUp?"
-    );
     const markerResponse = await request(server.server)
       .post("/v1/papers/1/users")
-      .set("Authorization", `Bearer ${accessToken}`)
+      .set("Authorization", ownerAccessToken)
       .send({
         email: "duplicate@u.nus.edu",
         role: PaperUserRole.Marker
@@ -135,7 +119,7 @@ describe("POST papers/:id/users", () => {
 
     const studentResponse = await request(server.server)
       .post("/v1/papers/1/users")
-      .set("Authorization", `Bearer ${accessToken}`)
+      .set("Authorization", ownerAccessToken)
       .send({
         email: "duplicate@u.nus.edu",
         role: PaperUserRole.Student
@@ -144,4 +128,56 @@ describe("POST papers/:id/users", () => {
   });
 
   // it("should create a new user if email does not exist", async () => {});
+});
+
+describe("POST papers/:id/scripts", () => {
+  it("should allow a Paper's Owner to access this route", async () => {
+    const response = await request(server.server)
+      .post(`/v1/papers/1/scripts`)
+      .set("Authorization", ownerAccessToken)
+      .send();
+    expect(response.status).not.toEqual(404);
+  });
+
+  it("should not allow a Paper's Marker to access this route", async () => {
+    const response = await request(server.server)
+      .post(`/v1/papers/1/scripts`)
+      .set("Authorization", markerAccessToken)
+      .send();
+    expect(response.status).toEqual(404);
+  });
+
+  it("should not allow a Paper's Student to access this route", async () => {
+    const response = await request(server.server)
+      .post(`/v1/papers/1/scripts`)
+      .set("Authorization", studentAccessToken)
+      .send();
+    expect(response.status).toEqual(404);
+  });
+});
+
+describe("GET papers/:id/scripts", () => {
+  it("should allow a Paper's Owner to access this route", async () => {
+    const response = await request(server.server)
+      .get(`/v1/papers/1/scripts`)
+      .set("Authorization", ownerAccessToken)
+      .send();
+    expect(response.status).not.toEqual(404);
+  });
+
+  it("should allow a Paper's Marker to access this route", async () => {
+    const response = await request(server.server)
+      .get(`/v1/papers/1/scripts`)
+      .set("Authorization", markerAccessToken)
+      .send();
+    expect(response.status).toEqual(404);
+  });
+
+  it("should allow a Paper's Student to access this route", async () => {
+    const response = await request(server.server)
+      .get(`/v1/papers/1/scripts`)
+      .set("Authorization", studentAccessToken)
+      .send();
+    expect(response.status).toEqual(404);
+  });
 });
