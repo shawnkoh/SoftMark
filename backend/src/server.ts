04aba9f68b99d68db0c332cb4453a66e2eea6c65
Shawn Koh <shawnkoh@me.com>
2019-10-30 18:31:11 +0800

    Add option to start express on another port

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -13,21 +13,21 @@ export class ApiServer {
   public connection: Connection | null = null;
   public server: Server | null = null;
 
-  async initialize() {
+  async initialize(port: number = 3000) {
     this.connection = await createConnection(ormconfig);
 
     const app = express();
-    app.use(bodyParser.json({ limit: '10mb' }));
-    app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
+    app.use(bodyParser.json({ limit: "10mb" }));
+    app.use(bodyParser.urlencoded({ extended: true, limit: "10mb" }));
     app.use(cors());
     app.use(helmet());
     if (process.env.NODE_ENV !== "test") {
-      console.log("Express server has started on port 3000.");
+      console.log(`Express server has started on port ${port}.`);
       app.use(morgan("dev"));
     }
     app.use("/", routes);
 
-    this.server = app.listen(3000);
+    this.server = app.listen(port);
   }
 
   async close() {
