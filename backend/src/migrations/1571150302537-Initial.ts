312e1a6ab5f9a0324e783553ccff36bb250e93e1
Shawn Koh <shawnkoh@me.com>
2019-11-09 18:17:57 +0800

    Remove password from admins

--- backend/src/migrations/1571150302537-Initial.ts
+++ backend/src/migrations/1571150302537-Initial.ts
@@ -5,11 +5,10 @@ import { User } from "../entities/User";
 export class Initial1571150302537 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
     await getRepository(User).save([
-      new User("shawnkoh@me.com", "setMeUp?"),
-      new User("shawnkoh@u.nus.edu", "setMeUp?"),
-      new User("ooimingsheng@gmail.com", "setMeUp?"),
-      new User("rlrh1996@gmail.com", "setMeUp?"),
-      new User("fungsiqi07@gmail.com", "setMeUp?")
+      new User("shawnkoh@me.com"),
+      new User("ooimingsheng@gmail.com"),
+      new User("rlrh1996@gmail.com"),
+      new User("fungsiqi07@gmail.com")
     ]);
   }
 
