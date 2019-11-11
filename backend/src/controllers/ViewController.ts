a947a4b54be900bcfa99330fa47718bc8c73da40
Shawn Koh <shawnkoh@me.com>
2019-11-12 03:33:19 +0800

    Add GET /scripts/:id/view - this is intended to replace GET /scripts/:id

--- backend/src/controllers/ViewController.ts
+++ backend/src/controllers/ViewController.ts
@@ -1,19 +1,195 @@
 import { addMinutes } from "date-fns";
 import { Request, Response } from "express";
 import _ from "lodash";
-import { Brackets, getRepository, getTreeRepository } from "typeorm";
+import {
+  Brackets,
+  getRepository,
+  getTreeRepository,
+  SelectQueryBuilder
+} from "typeorm";
 import { PageQuestion } from "../entities/PageQuestion";
 import { Question } from "../entities/Question";
 import QuestionTemplate from "../entities/QuestionTemplate";
-import { ScriptViewData, PageViewData, QuestionViewData } from "../types/view";
 import { PaperUserRole } from "../types/paperUsers";
 import { AccessTokenSignedPayload } from "../types/tokens";
+import { PageViewData, QuestionViewData, ScriptViewData } from "../types/view";
 import { allowedRequester } from "../utils/papers";
+
+function selectQuestionViewData<T>(queryBuilder: SelectQueryBuilder<T>) {
+  return queryBuilder
+    .select("question.id", "id")
+    .addSelect("questionTemplate.name", "name")
+    .addSelect("mark.score", "score")
+    .addSelect("questionTemplate.score", "maxScore")
+    .addSelect("questionTemplate.topOffset", "topOffset")
+    .addSelect("questionTemplate.leftOffset", "leftOffset");
+}
+
+function getRootQuestion(scriptId: number) {
+  const queryBuilder = getRepository(Question)
+    .createQueryBuilder("question")
+    .where("question.discardedAt IS NULL")
+    .innerJoin(
+      "question.script",
+      "script",
+      "script.id = :id AND script.discardedAt IS NULL",
+      { id: scriptId }
+    )
+    .leftJoin("script.student", "student", "student.discardedAt IS NULL")
+    .innerJoin(
+      "question.questionTemplate",
+      "questionTemplate",
+      "questionTemplate.discardedAt IS NULL AND questionTemplate.parentQuestionTemplateId IS NULL"
+    )
+    .leftJoin("question.marks", "mark", "mark.discardedAt IS NULL");
+
+  return selectQuestionViewData(queryBuilder)
+    .addSelect("student.matriculationNumber", "matriculationNumber")
+    .addSelect("script.paperId", "paperId")
+    .addSelect("questionTemplate.id", "questionTemplateId");
+
+  // consider showing all the root questions
+  // TODO: select proper first question
+}
+
+function getDescendantQuestionTemplates(
+  questionTemplate: QuestionTemplate | number
+) {
+  return getTreeRepository(QuestionTemplate)
+    .createDescendantsQueryBuilder(
+      "questionTemplate",
+      "questionTemplateClosure",
+      questionTemplate as QuestionTemplate // providing a number works too
+    )
+    .andWhere("questionTemplate.discardedAt IS NULL")
+    .andWhere("questionTemplate.parentQuestionTemplateId IS NOT NULL");
+}
+
+function getDescendantQuestions(descendantQuestionTemplateIds: number[]) {
+  const queryBuilder = getRepository(Question)
+    .createQueryBuilder("question")
+    .where("question.questionTemplateId IN (:...ids)", {
+      ids: descendantQuestionTemplateIds
+    })
+    .andWhere("question.discardedAt IS NULL")
+    .innerJoin(
+      "question.questionTemplate",
+      "questionTemplate",
+      "questionTemplate.discardedAt IS NULL"
+    )
+    .leftJoin("question.marks", "mark", "mark.discardedAt IS NULL");
+
+  return selectQuestionViewData(queryBuilder);
+}
+
+function getPagesData(questionIds: number[]) {
+  return getRepository(PageQuestion)
+    .createQueryBuilder("pageQuestion")
+    .where("pageQuestion.discardedAt IS NULL")
+    .andWhere("pageQuestion.questionId IN (:...ids)", { ids: questionIds })
+    .innerJoin("pageQuestion.page", "page")
+    .select("page.id", "id")
+    .addSelect("page.pageNo", "pageNo")
+    .addSelect("page.imageUrl", "imageUrl")
+    .leftJoin("page.annotations", "annotation")
+    .addSelect("annotation.id", "annotationId")
+    .addSelect("annotation.layer", "layer");
+}
+
+function getPages(pagesData: any): PageViewData[] {
+  return _.chain(pagesData)
+    .uniqBy("id")
+    .groupBy("id")
+    .map((array, id) => {
+      const questionIds = Array.from(
+        new Set(array.map(element => element["questionId"]))
+      );
+      const annotations = array
+        .filter(
+          element =>
+            "annotationId" in element &&
+            "layer" in element &&
+            element["annotationId"] !== null &&
+            element["annotationId"] !== undefined &&
+            element["layer"] !== null &&
+            element["layer"] !== undefined
+        )
+        .map(element => ({
+          id: element["annotationId"],
+          layer: element["layer"]
+        }));
+      return {
+        id: parseInt(id),
+        pageNo: array[0].pageNo,
+        imageUrl: array[0].imageUrl,
+        questionIds,
+        annotations
+      };
+    })
+    .value();
+}
+
+export async function viewScript(request: Request, response: Response) {
+  const payload = response.locals.payload as AccessTokenSignedPayload;
+  const requesterId = payload.userId;
+  const scriptId = Number(request.params.id);
+
+  const rootQuestionData = await getRootQuestion(scriptId).getRawOne();
+  if (!rootQuestionData) {
+    response.sendStatus(404);
+    return;
+  }
+  const {
+    paperId,
+    matriculationNumber,
+    questionTemplateId,
+    ...rootQuestion
+  } = rootQuestionData;
+  const allowed = await allowedRequester(
+    requesterId,
+    paperId,
+    PaperUserRole.Student
+  );
+  if (!allowed) {
+    response.sendStatus(404);
+    return;
+  }
+
+  const descendantQuestionTemplates = await getDescendantQuestionTemplates(
+    questionTemplateId
+  ).getMany();
+
+  const descendantQuestionTemplateIds = descendantQuestionTemplates.map(
+    descendant => descendant.id
+  );
+
+  const descendantQuestions = await getDescendantQuestions(
+    descendantQuestionTemplateIds
+  ).getRawMany();
+
+  const questionIds = descendantQuestions.map(descendant => descendant.id);
+  questionIds.push(rootQuestion.id);
+
+  const pagesData = await getPagesData(questionIds).getRawMany();
+
+  const pages = getPages(pagesData);
+
+  const data: ScriptViewData = {
+    rootQuestion,
+    matriculationNumber,
+    descendantQuestions,
+    pages
+  };
+
+  response.status(200).json(data);
+}
 
 /**
  * IMPORTANT NOTE
  * Since this is a MVP, this controller assumes that your allocation is on the root question
  * It will NOT recursively get inherited allocations
+ *
+ * TODO: Make use of the query builder functions above to shorten this code
  */
 export async function questionToMark(request: Request, response: Response) {
   const payload = response.locals.payload as AccessTokenSignedPayload;
@@ -124,12 +300,6 @@ export async function questionToMark(request: Request, response: Response) {
     )
     .andWhere("questionTemplate.discardedAt is null")
     .andWhere("questionTemplate.parentQuestionTemplateId is not null")
-    // TODO: Not sure how to select the data via their descendantsQueryBuilder
-    // .addSelect("questionTemplate.id", "id")
-    // .select("questionTemplate.name", "name")
-    // .addSelect("questionTemplate.score", "maxScore")
-    // .addSelect("questionTemplate.topOffset", "topOffset")
-    // .addSelect("questionTemplate.leftOffset", "leftOffset")
     .getMany();
 
   // prettier-ignore
@@ -149,7 +319,6 @@ export async function questionToMark(request: Request, response: Response) {
       .getRawOne();
     const { id, score } = question;
       
-    // Temporary workaround for line 131
     const descendantQuestion: QuestionViewData = {
       id,
       name: descendant.name,
@@ -165,54 +334,11 @@ export async function questionToMark(request: Request, response: Response) {
   const questionIds = descendantQuestions.map(child => child.id);
   questionIds.push(rootQuestion.id);
 
-  const pagesData = await getRepository(PageQuestion)
-    .createQueryBuilder("pageQuestion")
-    .where("pageQuestion.discardedAt IS NULL")
-    .andWhere("pageQuestion.questionId IN (:...ids)", { ids: questionIds })
-    .innerJoin("pageQuestion.page", "page")
-    .select("page.id", "id")
-    .addSelect("page.pageNo", "pageNo")
-    .addSelect("page.imageUrl", "imageUrl")
-    .leftJoin(
-      "page.annotations",
-      "annotation",
-      "annotation.paperUserId = :id",
-      { id: requester.id }
-    )
-    .addSelect("annotation.id", "annotationId")
-    .addSelect("annotation.layer", "layer")
+  const pagesData = await getPagesData(questionIds)
+    .andWhere("annotation.paperUserId = :id", { id: requester.id })
     .getRawMany();
 
-  const pages: PageViewData[] = _.chain(pagesData)
-    .uniqBy("id")
-    .groupBy("id")
-    .map((array, id) => {
-      const questionIds = Array.from(
-        new Set(array.map(element => element["questionId"]))
-      );
-      const annotations = array
-        .filter(
-          element =>
-            "annotationId" in element &&
-            "layer" in element &&
-            element["annotationId"] !== null &&
-            element["annotationId"] !== undefined &&
-            element["layer"] !== null &&
-            element["layer"] !== undefined
-        )
-        .map(element => ({
-          id: element["annotationId"],
-          layer: element["layer"]
-        }));
-      return {
-        id: parseInt(id),
-        pageNo: array[0].pageNo,
-        imageUrl: array[0].imageUrl,
-        questionIds,
-        annotations
-      };
-    })
-    .value();
+  const pages: PageViewData[] = getPages(pagesData);
 
   const data: ScriptViewData = {
     matriculationNumber,
