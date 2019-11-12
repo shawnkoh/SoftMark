e6036d624beb3e74a6d330dbb6dc25ead290ab59
Shawn Koh <shawnkoh@me.com>
2019-11-12 19:02:47 +0800

    Fix missing usePapers (#124)

--- frontend/src/AuthenticatedApp/paperSetup/PaperSetup.tsx
+++ frontend/src/AuthenticatedApp/paperSetup/PaperSetup.tsx
@@ -1,23 +1,20 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import usePaper from "../../contexts/PaperContext";
 import ScriptMapping from "./ScriptMapping";
 import ScriptTemplateView from "./ScriptTemplateView";
 
 const PaperSetup: React.FC = () => {
-  const paper = usePaper();
-
   const BASE_URL = "/papers/:paper_id/set_up";
 
   // Sub routes
   const scriptTemplateViewRoute = (
     <Route exact path={`${BASE_URL}/script_template`}>
-      <ScriptTemplateView paper={paper} />
+      <ScriptTemplateView />
     </Route>
   );
   const scriptMappingRoute = (
     <Route exact path={`${BASE_URL}/script_mapping`}>
-      <ScriptMapping paper={paper} />
+      <ScriptMapping />
     </Route>
   );
 
--- frontend/src/AuthenticatedApp/paperSetup/ScriptMapping.tsx
+++ frontend/src/AuthenticatedApp/paperSetup/ScriptMapping.tsx
@@ -1,17 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { RouteComponentProps, withRouter } from "react-router";
-
-import api from "../../api";
-import { PaperData } from "backend/src/types/papers";
-import { PaperUserListData } from "backend/src/types/paperUsers";
-import { ScriptListData } from "backend/src/types/scripts";
-
-import { Container, Tab, Tabs } from "@material-ui/core";
+import { Tab, Tabs } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-
+import React from "react";
 import TabPanel from "../../components/tables/TabPanel";
-import ScriptsTable from "./components/tables/ScriptsTable";
 import Header from "./components/headers/PaperSetupHeader";
+import ScriptsTable from "./components/tables/ScriptsTable";
 import StudentsTable from "./components/tables/StudentsTable";
 
 const useStyles = makeStyles(theme => ({
@@ -22,13 +14,7 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-interface OwnProps {
-  paper: PaperData;
-}
-
-type Props = OwnProps & RouteComponentProps;
-
-const ScriptMapping: React.FC<Props> = ({ paper, match: { params } }) => {
+const ScriptMapping: React.FC = () => {
   const classes = useStyles();
 
   const [tabValue, setTabValue] = React.useState(0);
@@ -36,10 +22,7 @@ const ScriptMapping: React.FC<Props> = ({ paper, match: { params } }) => {
 
   return (
     <>
-      <Header
-        paper={paper}
-        title="Map student scripts to student list / nominal roll"
-      />
+      <Header title="Map student scripts to student list / nominal roll" />
       <div>
         <Tabs
           value={tabValue}
@@ -51,14 +34,14 @@ const ScriptMapping: React.FC<Props> = ({ paper, match: { params } }) => {
         </Tabs>
 
         <TabPanel value={tabValue} index={0}>
-          <ScriptsTable paper={paper} />
+          <ScriptsTable />
         </TabPanel>
         <TabPanel value={tabValue} index={1}>
-          <StudentsTable paper={paper} />
+          <StudentsTable />
         </TabPanel>
       </div>
     </>
   );
 };
 
-export default withRouter(ScriptMapping);
+export default ScriptMapping;
