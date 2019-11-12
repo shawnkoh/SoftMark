85fb924812c9a7192b0e2e7a1c71e6ca5a5c5e7e
Shawn Koh <shawnkoh@me.com>
2019-11-12 12:34:01 +0800

    View scripts (#120)
    * Rename GradingData to ScriptViewData
    
    * Add GET /scripts/:id/view - this is intended to replace GET /scripts/:id
    
    * Refactor ViewController.questionToMark

--- backend/src/routes/api/scripts.ts
+++ backend/src/routes/api/scripts.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import * as ScriptsController from "../../controllers/ScriptsController";
+import * as ViewController from "../../controllers/ViewController";
 import { checkBearerToken } from "../../middlewares/checkBearerToken";
 import { BearerTokenType } from "../../types/tokens";
 
@@ -7,6 +8,7 @@ export const router = Router();
 
 router.use(checkBearerToken(BearerTokenType.AccessToken));
 router.get("/:id", ScriptsController.show);
+router.get("/:id/view", ViewController.viewScript);
 router.delete("/:id", ScriptsController.discard);
 router.patch("/:id/undiscard", ScriptsController.undiscard);
 router.patch("/:id", ScriptsController.update);
