9cbbe23ece665ff852d30fb42712e902d5bf8398
Rayner Lim <rlrh1996@gmail.com>
2019-11-14 12:19:46 +0800

    Add extra layer of confirmation to DeletePaperModal

--- frontend/src/AuthenticatedApp/PaperViewPage/components/DeletePaperModal.tsx
+++ frontend/src/AuthenticatedApp/PaperViewPage/components/DeletePaperModal.tsx
@@ -5,51 +5,85 @@ import {
   Button,
   Dialog,
   DialogContent,
-  DialogTitle,
-  IconButton,
-  Grid
+  DialogContentText,
+  DialogActions,
+  TextField
 } from "@material-ui/core";
-import Clear from "@material-ui/icons/Clear";
 import { PaperListData } from "backend/src/types/papers";
 import { toast } from "react-toastify";
-import ConfirmationDialog from "../../../components/dialogs/ConfirmationDialog";
+import CustomDialogTitle from "../../../components/dialogs/DialogTitleWithCloseButton";
 
 interface OwnProps {
   paper: PaperListData;
   render: any;
 }
 
-type Props = RouteComponentProps & OwnProps;
+type Props = OwnProps & RouteComponentProps;
 
-const DeleteStudentModal: React.FC<Props> = props => {
-  const { render, paper } = props;
-  const { name } = paper;
+const DeletePaperModal: React.FC<Props> = props => {
+  const { paper, render } = props;
+  const { id, name } = paper;
+
   const [isOpen, setIsOpen] = useState(false);
   const toggleVisibility = () => setIsOpen(!isOpen);
 
+  const [text, setText] = useState<string>("");
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setText(event.target.value);
+  };
+  useEffect(() => {
+    if (!isOpen) setText("");
+  }, [isOpen]);
+
+  const handleDelete = () => {
+    api.papers
+      .discardPaper(id)
+      .then(() => {
+        toast.success(`${name} has been deleted successfully.`);
+        props.history.push(`/`);
+      })
+      .catch(errors => {
+        toast.error(`${name} could not be deleted.`);
+      });
+    toggleVisibility();
+  };
+
   return (
     <>
-      <ConfirmationDialog
-        title={`Delete paper "${name}".`}
-        message={`This action is irreversible. Do you still want to delete?`}
-        open={isOpen}
-        handleClose={toggleVisibility}
-        handleConfirm={async () => {
-          api.papers
-            .discardPaper(paper.id)
-            .then(() => {
-              toast.success(`Paper "${name}" has been deleted successfully.`);
-              props.history.push(`/`);
-            })
-            .catch(errors => {
-              toast.error(`Paper "${name}" could not be deleted.`);
-            });
-          toggleVisibility();
-        }}
-      />
+      <Dialog open={isOpen} onClose={toggleVisibility}>
+        <CustomDialogTitle id="alert-dialog-title" onClose={toggleVisibility}>
+          Delete {name} paper
+        </CustomDialogTitle>
+        <DialogContent dividers>
+          <DialogContentText>
+            This action is irreversible! Do you want to delete the {name} paper?
+          </DialogContentText>
+          <TextField
+            label={`Enter "${paper.name}" here`}
+            margin="normal"
+            variant="outlined"
+            value={text}
+            onChange={handleChange}
+            style={{ width: "100%" }}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={toggleVisibility} color="primary">
+            Cancel
+          </Button>
+          <Button
+            onClick={handleDelete}
+            disabled={text !== name}
+            color="primary"
+            autoFocus
+          >
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
       {render(toggleVisibility)}
     </>
   );
 };
 
-export default withRouter(DeleteStudentModal);
+export default withRouter(DeletePaperModal);
