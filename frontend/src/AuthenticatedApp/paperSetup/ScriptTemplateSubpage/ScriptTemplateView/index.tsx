be8bc78443483f597b59a31027720227551fb6ed
Shawn Koh <shawnkoh@me.com>
2019-11-23 23:18:02 +0800

    Revert "Attempt to fix touch on iPad (#217)"
    This reverts commit 868a18b0d40f023d2b391c9b380a37777e0dd9ee.

--- frontend/src/AuthenticatedApp/paperSetup/ScriptTemplateSubpage/ScriptTemplateView/index.tsx
+++ frontend/src/AuthenticatedApp/paperSetup/ScriptTemplateSubpage/ScriptTemplateView/index.tsx
@@ -1,7 +1,8 @@
 import { Drawer, List } from "@material-ui/core";
 import React from "react";
 import { DndProvider } from "react-dnd";
-import TouchBackend from "react-dnd-touch-backend";
+import MultiBackend from "react-dnd-multi-backend";
+import HTML5toTouch from "react-dnd-multi-backend/dist/esm/HTML5toTouch";
 import useScriptSetup from "../../context/ScriptSetupContext";
 import PageTemplateView from "../PageTemplateView";
 import Header from "./PaperSetupHeaderStatic";
@@ -36,7 +37,7 @@ const ScriptTemplateView: React.FC = () => {
       </Drawer>
       <main className={classes.content}>
         <div className={classes.toolbar} />
-        <DndProvider backend={TouchBackend}>
+        <DndProvider backend={MultiBackend} options={HTML5toTouch}>
           {scriptTemplateSetupData.pageTemplates.map(pageTemplate => (
             <PageTemplateView
               key={pageTemplate.id}
