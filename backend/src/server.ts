33d1bc1bb236e1647dc534ae9b9697798d112cc9
Shawn Koh <shawnkoh@me.com>
2019-11-24 22:49:40 +0800

    Add sentry (#230)

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,6 +8,7 @@ import "reflect-metadata";
 import { Connection, createConnection } from "typeorm";
 import ormconfig from "../ormconfig";
 import routes from "./routes";
+const Sentry = require("@sentry/node");
 
 const corsOptions: CorsOptions = {
   origin: process.env.NODE_ENV === "production" ? /.*softmark\.io.*/ : "*"
@@ -21,6 +22,10 @@ export class ApiServer {
     this.connection = await createConnection(ormconfig);
 
     const app = express();
+    Sentry.init({
+      dsn: "https://5b635a518aa84ef592e783e2436ad7f4@sentry.io/1832519"
+    });
+    app.use(Sentry.Handlers.requestHandler());
     app.use(bodyParser.json({ limit: "20mb" }));
     app.use(bodyParser.urlencoded({ extended: true, limit: "20mb" }));
     app.use(cors(corsOptions));
@@ -30,6 +35,7 @@ export class ApiServer {
       app.use(morgan("dev"));
     }
     app.use("/", routes);
+    app.use(Sentry.Handlers.errorHandler());
 
     this.server = app.listen(port);
     this.server.timeout = 1200000;
