c92791f9af6ce1802bcdab3e5d3ca36863aa39ec
ooimingsheng <46950269+ooimingsheng@users.noreply.github.com>
2019-11-27 16:32:15 +0700

    Revert "remove-annotator-rerender (#291)"
    This reverts commit 3e1f84f463e55455ae6b9464d4f9a0dc8a2c8700.

--- backend/src/controllers/QuestionTemplatesController.ts
+++ backend/src/controllers/QuestionTemplatesController.ts
@@ -388,9 +388,10 @@ export async function rootQuestionTemplates(
       const descendants: QuestionTemplate[] = await getTreeRepository(
         QuestionTemplate
       ).findDescendants(rootQuestionTemplate);
-      rootQuestionTemplate.score = descendants
-        .map(QuestionTemplate => QuestionTemplate.score || 0)
-        .reduce((a, b) => a + b, 0);
+      rootQuestionTemplate.score =
+        descendants
+          .map(QuestionTemplate => QuestionTemplate.score || 0)
+          .reduce((a, b) => a + b, 0);
       return rootQuestionTemplate;
     })
   );
