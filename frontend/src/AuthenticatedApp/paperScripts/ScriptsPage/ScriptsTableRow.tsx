cf4add12e6980793a498fa26c3003c169e1dadca
Shawn Koh <shawnkoh@me.com>
2019-12-02 00:14:54 +0800

    Update ScriptsTableRow

--- frontend/src/AuthenticatedApp/paperScripts/ScriptsPage/ScriptsTableRow.tsx
+++ frontend/src/AuthenticatedApp/paperScripts/ScriptsPage/ScriptsTableRow.tsx
@@ -9,7 +9,7 @@ import CancelRounded from "@material-ui/icons/CancelRounded";
 import CheckRounded from "@material-ui/icons/CheckRounded";
 import { ScriptListData } from "backend/src/types/scripts";
 import useScriptTemplate from "contexts/ScriptTemplateContext";
-import React, { useState } from "react";
+import React from "react";
 import { useRouteMatch } from "react-router";
 import { Link } from "react-router-dom";
 import MarkWhichQuestionModal from "./MarkWhichQuestionModal";
@@ -22,32 +22,31 @@ interface Props {
 }
 
 const ScriptsTableRow: React.FC<Props> = props => {
+  const { script, questionTemplates } = props;
   const classes = useStyles();
   const { scriptTemplate } = useScriptTemplate();
   const totalMarks = scriptTemplate ? scriptTemplate.totalMarks : 0;
   const { url } = useRouteMatch()!;
 
-  const [script, setScript] = useState(props.script);
-  const { student, filename, id, awardedMarks, publishedDate } = script;
-  let matriculationNumber = "-";
-  let name = "-";
-  let email = "";
-  if (student) {
-    matriculationNumber = student.matriculationNumber || matriculationNumber;
-    const { user } = student;
-    name = user.name || name;
-    email = user.email;
-  }
+  const {
+    studentName,
+    studentEmail,
+    matriculationNumber,
+    filename,
+    id,
+    awardedMarks,
+    publishedDate
+  } = script;
 
   return (
     <TableRow>
       <TableCell>{id}</TableCell>
       <TableCell>{filename}</TableCell>
-      <TableCell>{matriculationNumber}</TableCell>
+      <TableCell>{matriculationNumber || "-"}</TableCell>
       <TableCell>
-        {name}
+        {studentName || "-"}
         <br />
-        {email}
+        {studentEmail}
       </TableCell>
       <TableCell>{`${awardedMarks} / ${totalMarks}`}</TableCell>
       <TableCell>
@@ -60,10 +59,10 @@ const ScriptsTableRow: React.FC<Props> = props => {
         </Container>
       </TableCell>
       <TableCell>
-        <Tooltip title={`Mark script of ${matriculationNumber}`}>
+        <Tooltip title={`Mark script of ${matriculationNumber || "-"}`}>
           <MarkWhichQuestionModal
             baseUrl={`${url}/${id}/mark`}
-            questionTemplates={props.questionTemplates}
+            questionTemplates={questionTemplates}
             render={toggleVisibility => (
               <Button
                 onClick={toggleVisibility}
@@ -76,7 +75,7 @@ const ScriptsTableRow: React.FC<Props> = props => {
             )}
           />
         </Tooltip>
-        <Tooltip title={`View script of ${matriculationNumber}`}>
+        <Tooltip title={`View script of ${matriculationNumber || "-"}`}>
           <Button
             component={Link}
             to={`${url}/${id}`}
@@ -87,7 +86,7 @@ const ScriptsTableRow: React.FC<Props> = props => {
             View
           </Button>
         </Tooltip>
-        <Tooltip title={`Download script of ${matriculationNumber}`}>
+        <Tooltip title={`Download script of ${matriculationNumber || "-"}`}>
           <Button
             component={Link}
             to={`${url}/${id}/save_script`}
