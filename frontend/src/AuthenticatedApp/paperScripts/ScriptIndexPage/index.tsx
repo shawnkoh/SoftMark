66ef1b345c512f0b7dd7b38325362e558f0fa289
Shawn Koh <shawnkoh@me.com>
2019-12-03 12:45:34 +0800

    Disable publish button when paper is published:

--- frontend/src/AuthenticatedApp/paperScripts/ScriptIndexPage/index.tsx
+++ frontend/src/AuthenticatedApp/paperScripts/ScriptIndexPage/index.tsx
@@ -206,6 +206,7 @@ const ScriptIndex: React.FC = () => {
                 color="primary"
                 onClick={toggleModal}
                 startIcon={<PublishIcon />}
+                disabled={!!paper.publishedDate}
               >
                 Publish
               </RoundedButton>
