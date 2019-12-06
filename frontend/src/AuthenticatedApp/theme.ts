7dc158966d702c4336b2d9fcea99b48516fbeae5
Shawn Koh <shawnkoh@me.com>
2019-12-06 22:24:55 +0800

    Adopt Si Qi's theme

--- frontend/src/AuthenticatedApp/theme.ts
+++ frontend/src/AuthenticatedApp/theme.ts
@@ -1,11 +1,9 @@
 import { createMuiTheme } from "@material-ui/core/styles";
-import lightBlue from "@material-ui/core/colors/lightBlue";
-import green from "@material-ui/core/colors/green";
 
 const theme = createMuiTheme({
   palette: {
-    primary: { main: lightBlue[600], contrastText: "#ffffff" },
-    secondary: { main: green[600], contrastText: "#ffffff" },
+    primary: { main: "#2C98F0", contrastText: "#ffffff" },
+    secondary: { main: "#81C784", contrastText: "#ffffff" },
     background: { default: "#fafafa", paper: "#ffffff" }
   }
 });
